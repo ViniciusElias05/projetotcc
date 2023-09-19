0d8f1baadfba87b53e288cb56acc4ad6dd786a01
Vinicius Elias <vini-mq@hotmail.com>
2023-09-19 11:04:12 -0300

    refactor ResourceExceptionHandler and add StandardError for the response error

--- src/main/java/com/viniciuselias/projetotcc/controller/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/viniciuselias/projetotcc/controller/exceptions/ResourceExceptionHandler.java
@@ -2,23 +2,32 @@ package com.viniciuselias.projetotcc.controller.exceptions;
 
 import com.viniciuselias.projetotcc.model.service.exceptions.DatabaseException;
 import com.viniciuselias.projetotcc.model.service.exceptions.ObjectNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.Instant;
 
 @RestControllerAdvice
 public class ResourceExceptionHandler {
 
     @ExceptionHandler(ObjectNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public String handleNotFoundException(ObjectNotFoundException e) {
-        return e.getMessage();
+    public StandardError handleNotFoundException(ObjectNotFoundException e, HttpServletRequest request) {
+        String error = "Resource not found";
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+        return err;
     }
 
     @ExceptionHandler(DatabaseException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public String handleBadRequestException(DatabaseException e) {
-        return e.getMessage();
+    public StandardError handleBadRequestException(DatabaseException e, HttpServletRequest request) {
+        String error = "Database error";
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+        return err;
     }
 }
